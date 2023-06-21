2ddceb31fa104baec7a006e3a0b2552e3af6f1d9
Krzysztof Nazar <s184698@student.pg.edu.pl>
2023-06-21 12:07:59 +0200

    adjust the admin views and models

--- fridgemanager/fridge/admin.py
+++ fridgemanager/fridge/admin.py
@@ -3,6 +3,27 @@ from .models import Product
 from .models import FridgeUser
 from .models import Fridge
 
-admin.site.register(Product)
-admin.site.register(FridgeUser)
-admin.site.register(Fridge)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'expire_date', 'amount', 'amount_unit')  # columns in admin view
+    ordering = ('name',)  # sorting; this comma must be here!
+    search_fields = ['name']
+
+
+@admin.register(Fridge)
+class FridgeAdmin(admin.ModelAdmin):
+    fields = (('name', 'created_date'), 'description', 'owners')  # creating page
+    list_display = ('name', 'created_date')  # main page view
+    list_filter = ('name', 'created_date')
+    ordering = ('name',)  # default sort order
+    search_fields = ['name']
+
+
+@admin.register(FridgeUser)
+class FridgeUserAdmin(admin.ModelAdmin):
+    fields = (('first_name', 'last_name'), 'email')
+    list_display = ('first_name', 'last_name', 'email')
+    list_filter = ('first_name', 'last_name')
+    ordering = ('first_name', 'last_name', )
+    search_fields = ('first_name', 'last_name', 'email')
--- fridgemanager/fridge/models.py
+++ fridgemanager/fridge/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class FridgeUser(models.Model):
@@ -13,7 +14,7 @@ class FridgeUser(models.Model):
 class Fridge(models.Model):
     name = models.CharField('Fridge name', max_length=100)
     created_date = models.DateTimeField('Registration date in the system')
-    description = models.CharField('Details about the fridge', max_length=200)
+    description = models.CharField('Details about the fridge', max_length=200, blank=True)
     owners = models.ManyToManyField(FridgeUser, blank=True)  # one to many
 
     def __str__(self):
@@ -21,11 +22,38 @@ class Fridge(models.Model):
 
 
 class Product(models.Model):
+    PIECES = 'Pieces'
+    GRAMS = 'Grams'
+    KILOGRAMS = 'Kilograms'
+    CARTON = "Carton"
+    BOTTLE = "Bottle"
+    JAR = "Jar"
+    CAN = "Can"
+    PACKAGE = "Package"
+    CONTAINER = "Container"
+
+    AMOUNT_UNIT_VALUES = [
+        (PIECES, 'Pieces'),
+        (GRAMS, 'Grams'),
+        (KILOGRAMS, 'Kilograms'),
+        (CARTON, 'Carton'),
+        (BOTTLE, 'Bottle'),
+        (JAR, 'Jar'),
+        (CAN, 'Can'),
+        (PACKAGE, 'Package'),
+        (CONTAINER, 'Container')
+    ]
+
     name = models.CharField('Product name', max_length=100)
     expire_date = models.DateTimeField('Expiration date of the product')
     amount = models.FloatField('Amount of the product stored')
+    amount_unit = models.CharField(
+        max_length=32,
+        choices=AMOUNT_UNIT_VALUES,
+        default=GRAMS,
+    )
     description = models.CharField('Details about the ingredient', max_length=100, blank=True, null=True)
-    fridge = models.ForeignKey(Fridge, on_delete=models.CASCADE, blank=True, null=True)  # one to many
+    fridge = models.ForeignKey(Fridge, on_delete=models.CASCADE, null=True)  # one to many
 
     def __str__(self):
         return self.name
