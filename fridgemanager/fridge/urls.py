464b11b45362e02f3069aac768ff18b6a29d3e85
Krzysztof Nazar <s184698@student.pg.edu.pl>
2023-06-21 10:51:21 +0200

    add urls, views and templates for products view

--- fridgemanager/fridge/urls.py
+++ fridgemanager/fridge/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('products', views.all_products, name='all_products'),
+
 ]
--- fridgemanager/fridge/views.py
+++ fridgemanager/fridge/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def home(request):
     return render(request, 'fridge/home.html', {})
+
+
+def all_products(request):
+    products = Product.objects.all()
+    print(products)
+    return render(request, 'fridge/products_list.html', {'products': products})
